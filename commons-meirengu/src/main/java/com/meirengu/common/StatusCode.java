c03f941c7164929e9343607a23f7996c0be82e1e
yuhaoyang <15011331617@163.com>
2017-03-13 20:01:29 +0800

    覆盖修改

--- commons-meirengu/src/main/java/com/meirengu/common/StatusCode.java
+++ commons-meirengu/src/main/java/com/meirengu/common/StatusCode.java
@@ -41,8 +41,12 @@ public class StatusCode {
     public static final int MOBILE_FORMAT_ERROR = 40016;
     //40100-40200//user_center
 
-    //40200-40300//news_cms
-
+    //50200-50300//news_cms
+    public static final int FEEDBACK_ERROR_INSERT = 50200;
+    public static final int BULLETIN_ERROR_INSERT = 50201;
+    public static final int FEEDBACK_CONTENT_OUTOF = 50202;
+    public static final int FEEDBACK_CONTENT_COUNT_OUTOF = 50203;
+    public static final int BULLETIN_ERROR_LIST = 50204;
     //40300-40400//medical_beauty
     public static final int MB_CORRECT = 40300;
     public static final int MB_ERROR_SELECT = 40301;
