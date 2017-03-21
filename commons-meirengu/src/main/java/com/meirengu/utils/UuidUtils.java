3d104a27487c7600927092bbbcb11ca9ecf657b7
huoyan403 <624475665@qq.com>
2017-03-21 11:04:21 +0800

    更新uuid生成

--- commons-meirengu/src/main/java/com/meirengu/utils/UuidUtils.java
+++ commons-meirengu/src/main/java/com/meirengu/utils/UuidUtils.java
@@ -2,7 +2,6 @@ package com.meirengu.utils;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -14,22 +13,19 @@ public class UuidUtils {
                     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
             };
 private static AtomicInteger InitId = new AtomicInteger();
-    public static Integer getShortUuid()
-    {
-
-        //return (int) (System.currentTimeMillis()-1000000000000L/1000)<<16|(InitId.addAndGet(1));
-        StringBuffer stringBuffer = new StringBuffer();
-        String uuid = UUID.randomUUID().toString().replace("-", "");
-        for (int i = 0; i < 8; i++)
-        {
-            String str      = uuid.substring(i * 4, i * 4 + 4);
-            int strInteger  = Integer.parseInt(str, 16);
-            stringBuffer.append(chars[strInteger % 0xA]);
-        }
-        stringBuffer.append((int)(Math.random()*10));
-        return Integer.valueOf(stringBuffer.toString());
+    public static Integer getShortUuid() {
+        return (int) (System.currentTimeMillis() - 1000000000000L / 1000) << 16L | (InitId.addAndGet(1));
     }
-
+//        StringBuffer stringBuffer = new StringBuffer();
+//        String uuid = UUID.randomUUID().toString().replace("-", "");
+//        for (int i = 0; i < 8; i++)
+//        {
+//            String str      = uuid.substring(i * 4, i * 4 + 4);
+//            int strInteger  = Integer.parseInt(str, 16);
+//            stringBuffer.append(chars[strInteger % 0xA]);
+//        }
+//        stringBuffer.append((int)(Math.random()*10));
+//        return Integer.valueOf(stringBuffer.toString());
 //    public static String getUuid(){
 //        return UUID.randomUUID().toString().replace("-", "");
 //    }
@@ -37,7 +33,7 @@ private static AtomicInteger InitId = new AtomicInteger();
         List<Integer> test01 = new ArrayList<Integer>();
         for(int i = 1; i<1000000; i++){
             test01.add(getShortUuid());
-            //System.err.println(getShortUuid());
+//            System.err.println(getShortUuid());
         }
 
         for(int i = 1; i<999999; i++){
