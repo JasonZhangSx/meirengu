1bf2800a6a31356709857f6d7ecbfb7c5cb593fd
huoyan403 <624475665@qq.com>
2017-03-21 11:17:27 +0800

    省市区合并为一张表

--- user_center/src/main/java/com/meirengu/uc/service/AddressService.java
+++ user_center/src/main/java/com/meirengu/uc/service/AddressService.java
@@ -1,12 +1,8 @@
 package com.meirengu.uc.service;
 
 import com.meirengu.uc.dao.AreasMapper;
-import com.meirengu.uc.dao.CitysMapper;
-import com.meirengu.uc.dao.ProvincesMapper;
 import com.meirengu.uc.dao.UserDao;
-import com.meirengu.uc.model.Areas;
-import com.meirengu.uc.model.Citys;
-import com.meirengu.uc.model.Provinces;
+import com.meirengu.uc.model.Area;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,32 +10,27 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 @Service
 public class AddressService {
-    @Autowired
-    ProvincesMapper dao;
 
     @Autowired
-    CitysMapper dao1;
-
-    @Autowired
-    AreasMapper dao2;
+    AreasMapper areasMapper;
     @Autowired
     UserDao userDao;
 
     // 查询出所有的省份
     @Transactional(readOnly = true)
-    public List<Provinces> showProvinceList() {
-        return dao.showProvinceList();
+    public List<Area> showProvinceList() {
+        return areasMapper.showProvinceList();
     }
     // 根据省id来查询所对应的城市名称
     @Transactional(readOnly = true)
-    public List<Citys> showCityListByPid(int pid) {
-        return dao1.showCityListByPid(pid);
+    public List<Area> showCityListByPid(int pid) {
+        return areasMapper.showCityListByPid(pid);
     }
 
     // 根据city的id来查询所有的区、县
     @Transactional(readOnly = true)
-    public List<Areas> showAreaListBycid(int cid) {
-        return dao2.showAreaListBycid(cid);
+    public List<Area> showAreaListBycid(int cid) {
+        return areasMapper.showAreaListBycid(cid);
     }
 
 }
