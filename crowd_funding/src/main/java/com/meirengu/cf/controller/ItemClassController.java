909db2f57a6f205c58e566d4a58a388e15d608e3
zhangjianxin <zhangjianxinsx@163.com>
2017-03-29 14:50:32 +0800

    后台接口完善

--- crowd_funding/src/main/java/com/meirengu/cf/controller/ItemClassController.java
+++ crowd_funding/src/main/java/com/meirengu/cf/controller/ItemClassController.java
@@ -1,19 +1,25 @@
 package com.meirengu.cf.controller;
 
+import com.meirengu.cf.common.Constants;
+import com.meirengu.cf.model.Item;
 import com.meirengu.cf.model.ItemClass;
+import com.meirengu.cf.model.Type;
 import com.meirengu.cf.service.ItemClassService;
 import com.meirengu.common.StatusCode;
 import com.meirengu.controller.BaseController;
 import com.meirengu.model.Page;
 import com.meirengu.model.Result;
 import com.meirengu.utils.StringUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -25,28 +31,146 @@ import java.util.Map;
 @RequestMapping("item_class")
 public class ItemClassController extends BaseController{
 
+    private static Logger LOGGER = LoggerFactory.getLogger(ItemClassController.class);
+
     @Autowired
     ItemClassService itemClassService;
 
-    @RequestMapping("list")
     @ResponseBody
-    public Result list(HttpServletRequest request){
-        int pageNum = 1;
-        int pageSize = 10;
-        if(!StringUtil.isEmpty(request.getParameter("page"))){
-            pageNum = Integer.parseInt(request.getParameter("page"));
-        }
-
-        if(!StringUtil.isEmpty(request.getParameter("per_page"))){
-            pageSize = Integer.parseInt(request.getParameter("per_page"));
+    @RequestMapping(method = RequestMethod.GET)
+    public Result list(@RequestParam(value = "per_page", required = false, defaultValue = "10") int pageSize,
+                       @RequestParam(value = "page", required = false, defaultValue = "1") int pageNum,
+                       @RequestParam(value = "is_page", required = false) boolean isPage,
+                       @RequestParam(value = "class_name", required = false) String className,
+                       @RequestParam(value = "flag", required = false) Integer flag,
+                       @RequestParam(value = "sortby", required = false) String sortBy,
+                       @RequestParam(value = "order", required = false) String order){
+        //默认不分页
+        if(StringUtil.isEmpty(isPage)){
+            isPage = false;
         }
 
         Map<String, Object> map = new HashMap<>();
-        Page<ItemClass> page = new Page<>();
-        page.setPageNow(pageNum);
-        page.setPageSize(pageSize);
-        page = itemClassService.getListByPage(page, map);
+        map.put("className", className);
+        map.put("flag", flag);
+        map.put("sortBy", sortBy);
+        map.put("order", order);
 
-        return super.setResult(StatusCode.OK, page, StatusCode.codeMsgMap.get(StatusCode.OK));
+        if(isPage){
+            Page<ItemClass> page = new Page<>();
+            page.setPageNow(pageNum);
+            page.setPageSize(pageSize);
+            page = itemClassService.getListByPage(page, map);
+            return super.setResult(StatusCode.OK, page, StatusCode.codeMsgMap.get(StatusCode.OK));
+        }else {
+            List<Map<String, Object>> list = itemClassService.getList(map);
+            return super.setResult(StatusCode.OK, list, StatusCode.codeMsgMap.get(StatusCode.OK));
+        }
+    }
+
+    /**
+     * 新增众筹项目分类
+     * @param className
+     * @param classSort
+     * @param classParentId
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.POST)
+    public Result insert(@RequestParam(value = "class_name", required = false) String className,
+                         @RequestParam(value = "class_sort", required = false) Integer classSort,
+                         @RequestParam(value = "class_parent_id", required = false) Integer classParentId){
+
+        ItemClass ic = setItemClass(null, className, classParentId, classSort, Constants.STATUS_YES);
+        try {
+            int insertNum = itemClassService.insert(ic);
+            if(insertNum == 1){
+                return super.setResult(StatusCode.OK, "", StatusCode.codeMsgMap.get(StatusCode.OK));
+            }else {
+                return super.setResult(StatusCode.ITEM_TYPE_INSERT_ERROR, "", StatusCode.codeMsgMap.get(StatusCode.ITEM_TYPE_INSERT_ERROR));
+            }
+        }catch (Exception e){
+            LOGGER.error(">> insert item class throw exception: {}", e);
+            return super.setResult(StatusCode.INTERNAL_SERVER_ERROR, "", StatusCode.codeMsgMap.get(StatusCode.INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    /**
+     * 获取众筹项目分类详情
+     * @param classId
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "{class_id}", method = RequestMethod.GET)
+    public Result detail(@PathVariable(value = "class_id", required = false)int classId){
+        try {
+            ItemClass ic = itemClassService.detail(classId);
+            return super.setResult(StatusCode.OK, ic, StatusCode.codeMsgMap.get(StatusCode.OK));
+        }catch (Exception e){
+            LOGGER.error(">> get item class detail throw exception: {}", e);
+            return super.setResult(StatusCode.INTERNAL_SERVER_ERROR, "", StatusCode.codeMsgMap.get(StatusCode.INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    /**
+     * 修改众筹项目分类信息
+     * @param classId
+     * @param className
+     * @param classSort
+     * @param flag
+     * @param classParentId
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.PUT)
+    public Result update(@RequestParam(value = "class_id", required = false)int classId,
+                         @RequestParam(value = "class_name", required = false)String className,
+                         @RequestParam(value = "class_sort", required = false)Integer classSort,
+                         @RequestParam(value = "flag", required = false)Integer flag,
+                         @RequestParam(value = "class_parent_id", required = false)Integer classParentId){
+
+        ItemClass ic = setItemClass(classId, className, classParentId, classSort, flag);
+        try {
+            int updateNum = itemClassService.update(ic);
+            if(updateNum == 1){
+                return super.setResult(StatusCode.OK, "", StatusCode.codeMsgMap.get(StatusCode.OK));
+            }else {
+                return super.setResult(StatusCode.ITEM_TYPE_UPDATE_ERROR, "", StatusCode.codeMsgMap.get(StatusCode.ITEM_TYPE_UPDATE_ERROR));
+            }
+        }catch (Exception e){
+            LOGGER.error(">> update item class throw exception: {}", e);
+            return super.setResult(StatusCode.INTERNAL_SERVER_ERROR, "", StatusCode.codeMsgMap.get(StatusCode.INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    /**
+     * 删除众筹项目分类
+     * @param classId
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "{class_id}", method = RequestMethod.DELETE)
+    public Result delete(@PathVariable(value = "class_id", required = false)int classId){
+        try {
+            int deleteNum = itemClassService.delete(classId);
+            if(deleteNum == 1){
+                return super.setResult(StatusCode.OK, "", StatusCode.codeMsgMap.get(StatusCode.OK));
+            }else {
+                return super.setResult(StatusCode.ITEM_TYPE_DELETE_ERROR, "", StatusCode.codeMsgMap.get(StatusCode.ITEM_TYPE_DELETE_ERROR));
+            }
+        }catch (Exception e){
+            LOGGER.error(">> delete item class detail throw exception: {}", e);
+            return super.setResult(StatusCode.INTERNAL_SERVER_ERROR, "", StatusCode.codeMsgMap.get(StatusCode.INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    private ItemClass setItemClass(Integer classId, String className, Integer classParentId, Integer classSort, Integer flag){
+        ItemClass ic = new ItemClass();
+        ic.setClassId(classId);
+        ic.setClassName(className);
+        ic.setClassParentId(classParentId);
+        ic.setClassSort(classSort);
+        ic.setFlag(flag);
+        return ic;
     }
 }
--- crowd_funding/src/main/java/com/meirengu/cf/controller/TypeController.java
+++ crowd_funding/src/main/java/com/meirengu/cf/controller/TypeController.java
@@ -1,10 +1,24 @@
 package com.meirengu.cf.controller;
 
+import com.meirengu.cf.model.PartnerClass;
+import com.meirengu.cf.model.Type;
+import com.meirengu.cf.service.PartnerClassService;
+import com.meirengu.cf.service.TypeService;
+import com.meirengu.common.StatusCode;
 import com.meirengu.controller.BaseController;
+import com.meirengu.model.Page;
+import com.meirengu.model.Result;
+import com.meirengu.utils.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 众筹类型表
@@ -17,5 +31,151 @@ public class TypeController extends BaseController{
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TypeController.class);
 
+    @Autowired
+    TypeService typeService;
 
+    /**
+     * 获取请求列表
+     * @param pageSize
+     * @param pageNum
+     * @param isPage
+     * @param typeName
+     * @param flag
+     * @param sortBy
+     * @param order
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET)
+    public Result list(@RequestParam(value = "per_page", required = false, defaultValue = "10") int pageSize,
+                       @RequestParam(value = "page", required = false, defaultValue = "1") int pageNum,
+                       @RequestParam(value = "is_page", required = false) boolean isPage,
+                       @RequestParam(value = "type_name", required = false) String typeName,
+                       @RequestParam(value = "flag", required = false) Integer flag,
+                       @RequestParam(value = "sortby", required = false, defaultValue = "create_time") String sortBy,
+                       @RequestParam(value = "order", required = false, defaultValue = "desc") String order){
+
+        //默认不分页
+        if(StringUtil.isEmpty(isPage)){
+            isPage = false;
+        }
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("typeName", typeName);
+        map.put("flag", flag);
+        map.put("sortBy", sortBy);
+        map.put("order", order);
+
+        if(isPage){
+            Page<Type> page = new Page<>();
+            page.setPageNow(pageNum);
+            page.setPageSize(pageSize);
+            page = typeService.getListByPage(page, map);
+            return super.setResult(StatusCode.OK, page, StatusCode.codeMsgMap.get(StatusCode.OK));
+        }else {
+            List<Map<String, Object>> list = typeService.getList(map);
+            return super.setResult(StatusCode.OK, list, StatusCode.codeMsgMap.get(StatusCode.OK));
+        }
+    }
+
+    /**
+     * 新增众筹分类
+     * @param typeName
+     * @param typeSort
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.POST)
+    public Result insert(@RequestParam(value = "type_name", required = false) String typeName,
+                         @RequestParam(value = "type_sort", required = false) Integer typeSort){
+
+        Type type = setType(null, typeName, typeSort, 255);
+        try {
+            int insertNum = typeService.insert(type);
+            if(insertNum == 1){
+                return super.setResult(StatusCode.OK, "", StatusCode.codeMsgMap.get(StatusCode.OK));
+            }else {
+                return super.setResult(StatusCode.TYPE_INSERT_ERROR, "", StatusCode.codeMsgMap.get(StatusCode.TYPE_INSERT_ERROR));
+            }
+        }catch (Exception e){
+            LOGGER.error(">> insert class throw exception: {}", e);
+            return super.setResult(StatusCode.INTERNAL_SERVER_ERROR, "", StatusCode.codeMsgMap.get(StatusCode.INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    /**
+     * 获取众筹分类详情
+     * @param typeId
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "{type_id}", method = RequestMethod.GET)
+    public Result detail(@PathVariable(value = "type_id", required = false)int typeId){
+        try {
+            Type type = typeService.detail(typeId);
+            return super.setResult(StatusCode.OK, type, StatusCode.codeMsgMap.get(StatusCode.OK));
+        }catch (Exception e){
+            LOGGER.error(">> get class detail throw exception: {}", e);
+            return super.setResult(StatusCode.INTERNAL_SERVER_ERROR, "", StatusCode.codeMsgMap.get(StatusCode.INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    /**
+     * 修改众筹分类信息
+     * @param typeId
+     * @param typeName
+     * @param typeSort
+     * @param flag
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.PUT)
+    public Result update(@RequestParam(value = "type_id", required = false)int typeId,
+                         @RequestParam(value = "type_name", required = false)String typeName,
+                         @RequestParam(value = "type_sort", required = false)Integer typeSort,
+                         @RequestParam(value = "flag", required = false)Integer flag){
+
+        Type type = setType(typeId, typeName, typeSort, flag);
+        try {
+            int updateNum = typeService.update(type);
+            if(updateNum == 1){
+                return super.setResult(StatusCode.OK, "", StatusCode.codeMsgMap.get(StatusCode.OK));
+            }else {
+                return super.setResult(StatusCode.TYPE_UPDATE_ERROR, "", StatusCode.codeMsgMap.get(StatusCode.TYPE_UPDATE_ERROR));
+            }
+        }catch (Exception e){
+            LOGGER.error(">> update class throw exception: {}", e);
+            return super.setResult(StatusCode.INTERNAL_SERVER_ERROR, "", StatusCode.codeMsgMap.get(StatusCode.INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    /**
+     * 删除众筹分类
+     * @param typeId
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "{type_id}", method = RequestMethod.DELETE)
+    public Result delete(@PathVariable(value = "type_id", required = false)int typeId){
+        try {
+            int deleteNum = typeService.delete(typeId);
+            if(deleteNum == 1){
+                return super.setResult(StatusCode.OK, "", StatusCode.codeMsgMap.get(StatusCode.OK));
+            }else {
+                return super.setResult(StatusCode.TYPE_DELETE_ERROR, "", StatusCode.codeMsgMap.get(StatusCode.TYPE_DELETE_ERROR));
+            }
+        }catch (Exception e){
+            LOGGER.error(">> delete class detail throw exception: {}", e);
+            return super.setResult(StatusCode.INTERNAL_SERVER_ERROR, "", StatusCode.codeMsgMap.get(StatusCode.INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    private Type setType(Integer typeId, String typeName, Integer typeSort, Integer flag){
+        Type type = new Type();
+        type.setTypeId(typeId);
+        type.setTypeName(typeName);
+        type.setTypeSort(typeSort);
+        type.setFlag(flag);
+        return type;
+    }
 }
