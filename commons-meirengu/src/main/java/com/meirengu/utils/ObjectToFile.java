740d70b876d3506d13ab56b5dd46bc342933462f
huoyan403 <624475665@qq.com>
2017-04-05 17:34:37 +0800

    地址修改

--- commons-meirengu/src/main/java/com/meirengu/utils/ObjectToFile.java
+++ commons-meirengu/src/main/java/com/meirengu/utils/ObjectToFile.java
@@ -4,8 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
