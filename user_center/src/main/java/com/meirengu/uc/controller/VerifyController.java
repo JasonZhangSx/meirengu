0b6f34b39e6702cf8e101e22aec6babd24867d53
huoyan403 <624475665@qq.com>
2017-04-12 11:30:05 +0800

    注册验证

--- user_center/src/main/java/com/meirengu/uc/controller/VerifyController.java
+++ user_center/src/main/java/com/meirengu/uc/controller/VerifyController.java
@@ -18,6 +18,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by huoyan403 on 3/24/2017.
  */
@@ -74,21 +77,15 @@ public class VerifyController extends BaseController {
                          @RequestParam(value = "token", required = true)String token) {
 
         try {
-            //判断有无token
-            if(!StringUtil.isEmpty(token)){
-                //判断token是否有效
-                if(!redisClient.existsObject(token)){
-                    return super.setResult(StatusCode.TOKEN_IS_TIMEOUT, null, StatusCode.codeMsgMap.get(StatusCode.TOKEN_IS_TIMEOUT));
-                }
+            //判断token是否有效
+            if(!redisClient.existsObject(token)){
+                return super.setResult(StatusCode.TOKEN_IS_TIMEOUT, null, StatusCode.codeMsgMap.get(StatusCode.TOKEN_IS_TIMEOUT));
             }
-            if(!StringUtil.isEmpty(bankPhone) && ValidatorUtil.isMobile(bankPhone)){
-
+            if(StringUtil.isEmpty(bankPhone) || ! ValidatorUtil.isMobile(bankPhone)){
+                return super.setResult(StatusCode.MOBILE_FORMAT_ERROR, null, StatusCode.codeMsgMap.get(StatusCode.MOBILE_FORMAT_ERROR));
             }
-            if(!StringUtil.isEmpty(idcard) && ValidatorUtil.isIDCard(idcard)){
-
-            }
-            if(!StringUtil.isEmpty(realname) && ValidatorUtil.isUsername(realname)){
-
+            if(StringUtil.isEmpty(idcard) || ! ValidatorUtil.isIDCard(idcard)){
+                return super.setResult(StatusCode.ID_CARD_IS_ERROR, null, StatusCode.codeMsgMap.get(StatusCode.ID_CARD_IS_ERROR));
             }
             if(userService.getBankIdCard(bankIdcard)){
                 return super.setResult(StatusCode.BANK_ID_CARD_IS_EXITS, null, StatusCode.
@@ -100,22 +97,11 @@ public class VerifyController extends BaseController {
                 if(times>Integer.parseInt(ConfigUtil.getConfig("VERIFY_TIMES"))){
                     return super.setResult(StatusCode.VETIFY_IS_NOT_ALLOWED, null, StatusCode.codeMsgMap.get(StatusCode.VETIFY_IS_NOT_ALLOWED));
                 }
-            }
-            redisClient.setObject("verify_"+userId,times+1,86400);
-            return verityService.verityUser(userId,bankCode,bankIdcard,bankPhone,idcard,realname,password,investConditions);
-            /*if(flag==StatusCode.OK){
-                return super.setResult(StatusCode.OK, null, StatusCode.codeMsgMap.get(StatusCode.OK));
-            }else if(flag == StatusCode.PAYMENT_RECORD_ERROR_BAOFU_PAY_RETURN_VALUE_ISNULL){
-                return super.setResult(StatusCode.PAYMENT_RECORD_ERROR_BAOFU_PAY_RETURN_VALUE_ISNULL, null, StatusCode.
-                        codeMsgMap.get(StatusCode.PAYMENT_RECORD_ERROR_BAOFU_PAY_RETURN_VALUE_ISNULL));
-            }
-            if(flag == StatusCode.PAYMENT_RECORD_ERROR_BAOFU_AUTH){
-                return super.setResult(StatusCode.PAYMENT_RECORD_ERROR_BAOFU_AUTH, null, StatusCode.
-                        codeMsgMap.get(StatusCode.PAYMENT_RECORD_ERROR_BAOFU_AUTH));
+                redisClient.setObject("verify_"+userId,times+1);
             }else{
-                return super.setResult(StatusCode.VETIFY_IS_ERROR, null, StatusCode.
-                        codeMsgMap.get(StatusCode.VETIFY_IS_ERROR));
-            }*/
+                redisClient.setObject("verify_"+userId,times+1,86400);
+            }
+            return verityService.verityUser(userId,bankCode,bankIdcard,bankPhone,idcard,realname,password,investConditions);
         }catch (Exception e){
             logger.info("VerifyController.vetify throws Exception:{}",e.getMessage());
             return super.setResult(StatusCode.INTERNAL_SERVER_ERROR, null, StatusCode.codeMsgMap.get(StatusCode.INTERNAL_SERVER_ERROR));
@@ -140,4 +126,34 @@ public class VerifyController extends BaseController {
             return super.setResult(StatusCode.UNKNOWN_EXCEPTION, null, StatusCode.codeMsgMap.get(StatusCode.UNKNOWN_EXCEPTION));
         }
     }
+
+
+    @RequestMapping(value = "register",method = {RequestMethod.POST})
+    public Result user(@RequestParam(value = "phone", required = true)String phone,
+                       @RequestParam(value = "invite_phone", required = false)String invitePhone){
+
+        try {
+            User user = userService.retrieveByPhone(phone);
+            User inviteUser = userService.retrieveByPhone(invitePhone);
+            Map map = new HashMap<>();
+            if(user==null && inviteUser==null){
+                map.put("msg","用户和邀请人都不存在！");
+                return super.setResult(StatusCode.OK, map, StatusCode.codeMsgMap.get(StatusCode.OK));
+            }
+            if(user==null){
+                map.put("msg","用户不存在！");
+                return super.setResult(StatusCode.OK, map, StatusCode.codeMsgMap.get(StatusCode.OK));
+            }
+            if(inviteUser==null){
+                map.put("msg","邀请人不存在！");
+                return super.setResult(StatusCode.OK, map, StatusCode.codeMsgMap.get(StatusCode.OK));
+            }
+            return super.setResult(StatusCode.OK, null, StatusCode.codeMsgMap.get(StatusCode.OK));
+        }catch (Exception e){
+            logger.info("VerifyController user Throws Exception :{}" ,e.getMessage());
+            return super.setResult(StatusCode.UNKNOWN_EXCEPTION, null, StatusCode.codeMsgMap.get(StatusCode.UNKNOWN_EXCEPTION));
+        }
+    }
+
+
 }
