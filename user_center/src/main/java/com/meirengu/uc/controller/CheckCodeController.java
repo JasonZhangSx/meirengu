35d94b3fbc8579ff4c53e9e1b6bc3c02b8d80904
huoyan403 <624475665@qq.com>
2017-04-12 14:38:21 +0800

    找回登陆密码 发送验证码序判断用户是否注册 把codeType提取到properties中

--- user_center/src/main/java/com/meirengu/uc/controller/CheckCodeController.java
+++ user_center/src/main/java/com/meirengu/uc/controller/CheckCodeController.java
@@ -56,17 +56,16 @@ public class CheckCodeController extends BaseController {
         try {
 
             if(redisClient.existsObject(type+"_"+mobile)){
-                return setResult(StatusCode.CHECK_CODE_SENDER_REFUSED, null, StatusCode.codeMsgMap.get(StatusCode
-                        .CHECK_CODE_SENDER_REFUSED));
+                return setResult(StatusCode.CHECK_CODE_SENDER_REFUSED, null, StatusCode.codeMsgMap.get(StatusCode.CHECK_CODE_SENDER_REFUSED));
             }
             //verify params
             if (StringUtils.isEmpty(mobile) || !ValidatorUtil.isMobile(mobile)) {
-                return setResult(StatusCode.MOBILE_FORMAT_ERROR, null, StatusCode.codeMsgMap.get(StatusCode
-                        .MOBILE_FORMAT_ERROR));
+                return setResult(StatusCode.MOBILE_FORMAT_ERROR, null, StatusCode.codeMsgMap.get(StatusCode.MOBILE_FORMAT_ERROR));
             }
             //注册  登陆
             // 找回登陆密码  找回交易密码 需要判断用户是否注册
-            if("1790347".equals(type)){
+            String codeType = ConfigUtil.getConfig("BACK_CODE_FOR_LOGIN");
+            if(codeType.equals(type)){
                 //验证手机号是否注册
                 User user = userService.retrieveByPhone(mobile);
                 if(StringUtil.isEmpty(user)){
