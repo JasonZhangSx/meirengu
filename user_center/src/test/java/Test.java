4f189464d21222575c26585e54d6698188e8776d
huoyan403 <624475665@qq.com>
2017-04-26 16:21:20 +0800

    邀请人导出

--- user_center/src/test/java/Test.java
+++ user_center/src/test/java/Test.java
@@ -1,16 +1,20 @@
 import com.meirengu.uc.utils.SerializableUtil;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by huoyan403 on 3/17/2017.
  */
 public class Test {
 
-    public static void main(String args[]){
-        System.out.println("lianzhang");
-        System.out.println("lianzhang".getBytes());
-        System.out.println("lianzhang".getBytes());
-        System.out.println(getByte("lianzhang"));
-        System.out.println(getByte("lianzhang"));
+    public static void main(String args[]) throws ParseException {
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        long timeStart=sdf.parse("2011-09-20 12:30:45").getTime();
+        System.out.println(timeStart);
+        Date date = new Date(1493049600000L);
+        System.out.println(sdf.format(date));
     }
 
 
