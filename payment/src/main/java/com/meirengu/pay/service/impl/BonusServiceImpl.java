2881e53f12379b25e74d2ca4707cd14cd743c677
yuhaoyang <15011331617@163.com>
2017-05-12 12:16:02 +0800

    邀请奖励分红修改为从oss读取文件

--- payment/src/main/java/com/meirengu/pay/service/impl/BonusServiceImpl.java
+++ payment/src/main/java/com/meirengu/pay/service/impl/BonusServiceImpl.java
@@ -6,6 +6,7 @@ import com.meirengu.pay.dao.PaymentInvitationBonusDao;
 import com.meirengu.pay.model.PaymentAccount;
 import com.meirengu.pay.model.PaymentInvitationBonus;
 import com.meirengu.pay.service.BonusService;
+import com.meirengu.pay.utils.HttpUtil;
 import com.meirengu.pay.utils.ResultUtil;
 import com.meirengu.utils.DateAndTime;
 import com.meirengu.utils.DateUtils;
@@ -39,14 +40,16 @@ public class BonusServiceImpl extends BaseServiceImpl implements BonusService {
             if (super.inviteUrl==null){
                 projectValue();
             }
-            read = new InputStreamReader(
-                    new FileInputStream(super.inviteUrl+"user."+ DateUtils.getCurrentDate()+".txt"),"utf-8");
-            BufferedReader bufferedReader = new BufferedReader(read);
-            for (String string : Arrays.asList(bufferedReader.readLine().split("\\|"))){
+//            read = new InputStreamReader(
+//                    new FileInputStream(super.inviteUrl+"user."+ DateUtils.getCurrentDate()+".txt"),"utf-8");
+//            BufferedReader bufferedReader = new BufferedReader(read);
+//            for (String string : Arrays.asList(bufferedReader.readLine().split("\\|"))){
+            String content = HttpUtil.httpGet(super.inviteUrl+"user."+DateUtils.getCurrentDate()+".txt","utf-8");
+            for (String string : content.split("\\|")){
                 String[] str = string.split(",");
-                if (str[3]!="1"){
-                    BigDecimal bigDecimal = new BigDecimal(str[2]);
-                    BigDecimal money = bigDecimal.multiply(BigDecimal.valueOf(0.005)).setScale(BigDecimal.ROUND_CEILING,BigDecimal.ROUND_HALF_UP);
+                if (!str[2].equals("1")){
+                    BigDecimal bigDecimal = new BigDecimal(str[1]);
+                    BigDecimal money = bigDecimal.multiply(BigDecimal.valueOf(0.01)).setScale(BigDecimal.ROUND_CEILING,BigDecimal.ROUND_HALF_UP);
                     for (int i=0;i<str.length-2;i++){
                         Integer userId = Integer.valueOf(str[i]);
                         PaymentAccount paymentAccount = paymentAccountDao.selectByUserId(userId);
@@ -54,20 +57,12 @@ public class BonusServiceImpl extends BaseServiceImpl implements BonusService {
                         PaymentInvitationBonus paymentInvitationBonus = new PaymentInvitationBonus();
                         paymentInvitationBonus.setUserId(userId);
                         paymentInvitationBonus.setPrincipal(money);
-                        if (i==0){
-                            paymentInvitationBonus.setInvestPrincipal(bigDecimal);
-                        }
+                        paymentInvitationBonus.setInvestPrincipal(bigDecimal);
                         paymentInvitationBonus.setInvestmentTime(DateAndTime.addDay(-4));
                         paymentInvitationBonusDao.insertSelective(paymentInvitationBonus);
                     }
                 }
             }
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         } catch (DocumentException e) {
             e.printStackTrace();
         }
