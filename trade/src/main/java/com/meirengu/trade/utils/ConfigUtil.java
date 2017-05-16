c8db96a1d1d3bc3dbe3376c1a4f617c2f7c89dca
maoruxin <c421771835@163.com>
2017-05-16 14:18:18 +0800

    erpproduct配置文件修改

--- trade/src/main/java/com/meirengu/trade/utils/ConfigUtil.java
+++ trade/src/main/java/com/meirengu/trade/utils/ConfigUtil.java
@@ -1,43 +1,52 @@
 package com.meirengu.trade.utils;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.URL;
 import java.util.Properties;
 
 /**
- * 通用参数工具类
+ * 配置工具类
  *
  * @author Marvin
- * @create 2017-01-12 下午3:22
+ * @create 2017-01-11 下午6:06
  */
 public class ConfigUtil {
 
-    protected static Properties props;
+    private static Properties props;
 
-    protected static InputStream in;
+    private static File configFile;
 
-    protected static long lastModified = 0l;
+    private static long lastModified = 0l;
 
-    protected static void init() {
+    private static void init() {
+        URL url = ConfigUtil.class.getClassLoader().getResource("trade.properties");
+        String file = url.getFile();
 
-        in = ConfigUtil.class.getClassLoader().getResourceAsStream("trade.properties");
+        configFile = new File(file);
 
         props = new Properties();
 
         load();
     }
 
-    protected static void load() {
+    private static void load() {
         try {
-            props.load(in);
+            props.load(new FileInputStream(configFile));
+            lastModified = configFile.lastModified();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     public static String getConfig(String key) {
-        if (props == null) {
+        if (props == null || configFile == null) {
             init();
+        }
+
+        if (lastModified != configFile.lastModified()) {
+            load();
         }
 
         return props.getProperty(key);
