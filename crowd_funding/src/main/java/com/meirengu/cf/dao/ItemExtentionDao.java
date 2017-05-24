cc125dd345f6deef88d013a080e61f4f03601b48
zhangjianxin <zhangjianxinsx@163.com>
2017-05-24 12:19:56 +0800

    新增项目扩展表、项目股东表

---
+++ crowd_funding/src/main/java/com/meirengu/cf/dao/ItemExtentionDao.java
@@ -0,0 +1,10 @@
+package com.meirengu.cf.dao;
+import com.meirengu.cf.model.ItemExtention;
+import com.meirengu.dao.BaseDao;
+/**
+ * ItemExtentionDao 
+ * @author 建新
+ * @create Tue May 23 14:41:52 CST 2017
+ */
+public interface ItemExtentionDao extends BaseDao<ItemExtention>{
+}
---
+++ crowd_funding/src/main/java/com/meirengu/cf/service/impl/ItemShareholderServiceImpl.java
@@ -0,0 +1,14 @@
+package com.meirengu.cf.service.impl;
+import com.meirengu.cf.model.ItemShareholder;
+import com.meirengu.cf.service.ItemShareholderService;
+import com.meirengu.service.impl.BaseServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * ItemShareholder服务实现层 
+ * @author 建新
+ * @create Tue May 23 14:41:52 CST 2017
+ */
+@Service
+public class ItemShareholderServiceImpl extends BaseServiceImpl<ItemShareholder> implements ItemShareholderService{
+}
