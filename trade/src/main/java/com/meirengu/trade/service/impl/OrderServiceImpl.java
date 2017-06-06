c85dbc65d175f69bfcb224d430d5acc88ac55131
maoruxin <c421771835@163.com>
2017-06-06 18:00:01 +0800

    订单详情返回字段修改

--- trade/src/main/java/com/meirengu/trade/service/impl/OrderServiceImpl.java
+++ trade/src/main/java/com/meirengu/trade/service/impl/OrderServiceImpl.java
@@ -269,10 +269,12 @@ public class OrderServiceImpl extends BaseServiceImpl<Order> implements OrderSer
                             JSONObject item = resultJson.getJSONObject("data");
                             String headerImagePath = item.getString("headerImage");
                             String itemStatus = item.getString("itemStatus");
+                            String itemProfile = item.getString("itemProfile");
                             int partnerId = item.getIntValue("partnerId");
                             map.put("headerImage", headerImagePath);
                             map.put("itemStatus", itemStatus);
                             map.put("partnerId", partnerId);
+                            map.put("itemProfile", itemProfile);
                         }
                     }
                 }
